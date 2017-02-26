5b8b07a5365cbe7e888bc65d54dbe3529c7809b8
mathbarre <mathieu.barre83@gmail.com>
2017-02-26 20:07:30 +0100

    Add files via upload

--- stacking.py
+++ stacking.py
@@ -21,11 +21,41 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import AdaBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, LassoCV, Lasso
 from math import sqrt
 from Ensemble import Ensemble
 
+
+class Discretize():
+    
+
+    
+    
+    def __init__(self,quantil):
+        self.quantil = quantil
+
+        
+
+    def fit(self,y):
+        q=self.quantil
+
+        s=np.arange(100/q+1)
+        s=s*q
+        self.table=np.percentile(y,s)
+        return(self.table)
+        
+        
+            
+    def transform(self,y):
+        table=self.table
+        for i in range(len(y)):
+            for j in range(len(table)):
+                if (y.iloc[i]<table[j]):
+                    y.iloc[i]=(table[j]+table[j-1])/2
+                    break
+        return(y)
 
 TARGET = 'SalePrice'
 NFOLDS = 4
@@ -37,30 +67,96 @@ SUBMISSION_FILE = 'sample_submission.csv'
 ## Load the data ##
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
-
 ntrain = train.shape[0]
 ntest = test.shape[0]
 
 ## Preprocessing ##
 
 y_train = np.log(train[TARGET])
-
-
 train.drop([TARGET], axis=1, inplace=True)
+"""
 
+
+train["IsRegularLotShape"] = (train["LotShape"] == "Reg") * 1
+
+    # Most properties are level; bin the other possibilities together
+    # as "not level".
+train["IsLandLevel"] = (train["LandContour"] == "Lvl") * 1
+
+    # Most land slopes are gentle; treat the others as "not gentle".
+train["IsLandSlopeGentle"] = (train["LandSlope"] == "Gtl") * 1
+
+    # Most properties use standard circuit breakers.
+train["IsElectricalSBrkr"] = (train["Electrical"] == "SBrkr") * 1
+
+    # About 2/3rd have an attached garage.
+train["IsGarageDetached"] = (train["GarageType"] == "Detchd") * 1
+
+    # Most have a paved drive. Treat dirt/gravel and partial pavement
+    # as "not paved".
+train["IsPavedDrive"] = (train["PavedDrive"] == "Y") * 1
+
+    # The only interesting "misc. feature" is the presence of a shed.
+train["HasShed"] = (train["MiscFeature"] == "Shed") * 1.  
+
+train['Yr'] = (train['YearBuilt']*train['YearRemodAdd'])
+train['Overall'] = (train['OverallQual']*(train['OverallCond']))
+train['Tt_Area'] = (train['TotalBsmtSF']/(train['GrLivArea']))
+train["RecentRemodel"] = (train["YearRemodAdd"] == train["YrSold"]) * 1
+train["VeryNewHouse"] = (train["YearBuilt"] == train["YrSold"]) * 1
+train["Remodeled"] = (train["YearRemodAdd"] != train["YearBuilt"]) * 1
+
+
+train["IsRegularLotShape"] = (train["LotShape"] == "Reg") * 1
+
+    # Most properties are level; bin the other possibilities together
+    # as "not level".
+train["IsLandLevel"] = (test["LandContour"] == "Lvl") * 1
+
+    # Most land slopes are gentle; treat the others as "not gentle".
+test["IsLandSlopeGentle"] = (test["LandSlope"] == "Gtl") * 1
+
+    # Most properties use standard circuit breakers.
+test["IsElectricalSBrkr"] = (test["Electrical"] == "SBrkr") * 1
+
+    # About 2/3rd have an attached garage.
+test["IsGarageDetached"] = (test["GarageType"] == "Detchd") * 1
+
+    # Most have a paved drive. Treat dirt/gravel and partial pavement
+    # as "not paved".
+test["IsPavedDrive"] = (test["PavedDrive"] == "Y") * 1
+
+    # The only interesting "misc. feature" is the presence of a shed.
+test["HasShed"] = (test["MiscFeature"] == "Shed") * 1.  
+
+test['Yr'] = (test['YearBuilt']*test['YearRemodAdd'])
+test['Overall'] = (test['OverallQual']*(test['OverallCond']))
+test['Tt_Area'] = (test['TotalBsmtSF']/(test['GrLivArea']))
+test["RecentRemodel"] = (test["YearRemodAdd"] == test["YrSold"]) * 1
+test["VeryNewHouse"] = (test["YearBuilt"] == test["YrSold"]) * 1
+test["Remodeled"] = (test["YearRemodAdd"] != test["YearBuilt"]) * 1
+"""
 
 
 
 all_data = pd.concat((train.loc[:,'MSSubClass':'SaleCondition'],
                       test.loc[:,'MSSubClass':'SaleCondition']))
 
-
+dis = Discretize(15)
+dis.fit(all_data['GrLivArea'].copy())
+all_data['discretize'] = pd.to_numeric(dis.transform(all_data['GrLivArea'].copy()))
+dis = Discretize(5)
+dis.fit(all_data['LotArea'].copy())
+all_data['discretize1'] = pd.to_numeric(dis.transform(all_data['LotArea'].copy()))
+dis = Discretize(3)
+dis.fit(all_data['YearBuilt'].copy())
+all_data['discretize2'] = pd.to_numeric(dis.transform(all_data['YearBuilt'].copy()))
 
 #log transform skewed numeric features:
 numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
 
-skewed_feats = train[numeric_feats].apply(lambda x: skew(x.dropna())) #compute skewness
-skewed_feats = skewed_feats[skewed_feats > 0.75]
+skewed_feats = all_data[numeric_feats].apply(lambda x: skew(x.dropna())) #compute skewness
+skewed_feats = skewed_feats[skewed_feats > 0.8]
 skewed_feats = skewed_feats.index
 
 all_data[skewed_feats] = np.log1p(all_data[skewed_feats])
@@ -76,6 +172,10 @@ all_data = all_data.fillna(all_data.mean())
 
 x_train = np.array(all_data[:train.shape[0]])
 x_test = np.array(all_data[train.shape[0]:])
+
+
+
+
 
 kf = KFold(ntrain, n_folds=NFOLDS, shuffle=True, random_state=SEED)
 
@@ -125,35 +225,36 @@ def get_oof(clf):
     return oof_train.reshape(-1, 1), oof_test.reshape(-1, 1)
 
 ab_params = {
+'base_estimator':DecisionTreeRegressor(max_depth = 14,max_features = 0.2),
 'n_estimators': 200,
-'learning_rate':1.0
+'learning_rate':1.0,
 
 }
     
 et_params = {
     'n_jobs': 16,
     'n_estimators': 100,
-    'max_features': 0.5,
-    'max_depth': 12,
+    'max_features': 0.2,
+    'max_depth': 13,
     'min_samples_leaf': 2,
 }
 
 rf_params = {
     'n_jobs': 16,
-    'n_estimators': 300,
+    'n_estimators': 200,
     'max_features': 0.2,
-    'max_depth': 12,
+    'max_depth': 14,
     'min_samples_leaf': 2,
 }
 
 xgb_params = {
     'seed': 0,
-    'colsample_bytree': 0.7,
+    'colsample_bytree': 0.6,
     'silent': 1,
-    'subsample': 0.7,
+    'subsample': 0.5,
     'learning_rate': 0.075,
     'objective': 'reg:linear',
-    'max_depth': 4,
+    'max_depth': 3,
     'num_parallel_tree': 1,
     'min_child_weight': 1,
     'eval_metric': 'rmse',
@@ -168,7 +269,7 @@ rd_params={
 
 
 ls_params={
-    'alpha': 0.00052
+    'alpha': 0.0005
 }
 
 ab = SklearnWrapper(clf = AdaBoostRegressor,seed=SEED,params=ab_params)
